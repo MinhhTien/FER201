7385fc01621fd0ead7d79494724eebc6095538fc
MinhhTien <92145479+MinhhTien@users.noreply.github.com>
2023-05-24 11:25:06 +0700

    Use react hook change theme

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from './ThemeContext';
 
 export default function Navigation() {
-    return (
-            <nav>
-                <ul>
-                    <li><a className='active' href='#home'>Home</a></li>
-                    <li><a href='#news'>News</a></li>
-                    <li><a href='#about'>About</a></li>
-                    <li><a href='#contact'>Contact</a></li>
-                </ul>
-            </nav>
-    )
+  const { theme, toggle, dark } = useContext(ThemeContext);
+  return (
+    <div>
+      <nav
+        style={{ backgroundColor: theme.background, color: theme.color }}
+      >
+        <ul>
+          <li>
+            <a className="active" href="#home">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#news">News</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+        <div style={{ position: 'relative' }}>
+          <a
+            className="switch-mode"
+            href="#"
+            onClick={toggle}
+            style={{
+              backgroundColor: theme.background,
+              color: theme.color,
+              outline: 'none',
+            }}
+          >
+            Switch Nav to {!dark ? 'Dark' : 'Light'}
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
 }
