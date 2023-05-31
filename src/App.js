e71af6abd6c002679655266afd5c53dbe5eda5a8
MinhhTien <92145479+MinhhTien@users.noreply.github.com>
2023-05-31 11:19:41 +0700

    Use react router dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import Navigation from './components/Navigation';
-import Main from './components/Main';
+import Players from './components/Players';
+import Detail from './components/Detail';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <Main />
+      <Routes>
+        <Route path='/' element={<Players />} />
+        <Route path='/detail/:id' element={<Detail />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
       <Footer />
     </div>
   );
--- src/components/Players.js
+++ src/components/Players.js
@@ -1,34 +1,37 @@
 import React, { useState } from 'react';
 import { PlayerList } from '../ListOfPlayers';
+import { Link } from 'react-router-dom';
 
 export default function Players() {
   const [player, setPlayer] = useState([]);
-    return (
-      <div className="container">
-        {PlayerList.map((player) => (
-          <div key={player.id} className="column">
-            <div className="card">
-              <img src={player.img} alt={player.name} />
-              <h3>{player.name}</h3>
-              <p className="title">{player.club}</p>
-              <p>
-                <button onClick={() => {setPlayer(player)}}>
-                  <a href='#popup1' id='openPopUp'>Detail</a>
-                </button>
-              </p>
-            </div>
-          </div>
-        ))}
-        <div id='popup1' className='overlay'>
-          <div className='popup'>
+  return (
+    <div className="container">
+      {PlayerList.map((player) => (
+        <div key={player.id} className="column">
+          <div className="card">
             <img src={player.img} alt={player.name} />
-            <h2>{player.name}</h2>
-            <a className='close' href='#'>&times;</a>
-            <div className='content'>
-              <p>{player.info}</p>
-            </div>
+            <h3>{player.name}</h3>
+            <p className="title">{player.club}</p>
+            <Link>
+              <p>
+                <button>Detail</button>
+              </p>
+            </Link>
+          </div>
+        </div>
+      ))}
+      <div id="popup1" className="overlay">
+        <div className="popup">
+          <img src={player.img} alt={player.name} />
+          <h2>{player.name}</h2>
+          <a className="close" href="#">
+            &times;
+          </a>
+          <div className="content">
+            <p>{player.info}</p>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
